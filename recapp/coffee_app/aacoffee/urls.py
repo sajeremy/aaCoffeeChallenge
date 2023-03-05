3d843ce537f5d5465d1ff0ee30fd33d4a208d056
“sajeremy” <9jsantiago4@gmail.com>
2023-03-05 16:42:44 -0400

    initial functioning api

--- recapp/coffee_app/aacoffee/urls.py
+++ recapp/coffee_app/aacoffee/urls.py
@@ -1,15 +1,29 @@
-from django.urls import include, path
-from rest_framework import routers
-from . import views
+# from django.conf.urls import url
+from django.urls import path, include
+from .views import (
+    CoffeePingView,
+    CoffeeListView
 
-router = routers.DefaultRouter()
-router.register(r'coffees', views.CoffeeViewSet)
-router.register(r'posts', views.PostViewSet)
+)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api/coffee/ping', CoffeePingView.as_view()),
+    path('api/coffee', CoffeeListView.as_view()),
 ]
+
+
+# # from django.urls import include, path
+# # from rest_framework import routers
+# # from . import views
+
+# # router = routers.DefaultRouter()
+# # router.register(r'coffees', views.CoffeeViewSet)
+# # router.register(r'posts', views.PostViewSet)
+
+# # urlpatterns = [
+# #     path('', include(router.urls)),
+# #     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+# # ]
 
 
 # from . import views
--- recapp/coffee_app/aacoffee/views.py
+++ recapp/coffee_app/aacoffee/views.py
@@ -1,30 +1,49 @@
-from rest_framework import viewsets
+from django.views import View
+from django.http import HttpResponse
+from rest_framework import permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.generics import get_object_or_404
 
 from .serializers import CoffeeSerializer, PostSerializer
 from .models import Coffee, Post
 
 
-class CoffeeViewSet(viewsets.ModelViewSet):
-    queryset = Coffee.objects.all().order_by('name')
-    serializer_class = CoffeeSerializer
+class CoffeePingView(APIView):
+    def get(self, req):
+        # return HttpResponse('Hello, World!')
+        return Response(status=status.HTTP_200_OK)
 
 
-class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all().order_by('title')
-    serializer_class = PostSerializer
+class CoffeeListView(APIView):
+    def get(self, req):
+        coffees = Coffee.objects.order_by('name')  # .all()
+        serializer = CoffeeSerializer(coffees, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-# from django.shortcuts import render
-# from django.http import HttpResponse
-# test
+    def post(self, req):
+        coffee_data = {
+            'name': req.data.get('name'),
+            'year': req.data.get('year'),
+            'caffine_content': req.data.get('caffine_content'),
+            'caffine_percentage': req.data.get('caffine_percentage')
+        }
+        serializer = CoffeeSerializer(data=coffee_data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+        # from rest_framework import viewsets
 
-# # def coffee(request):
-# #     return HttpResponse("Hello world!")
+        # from .serializers import CoffeeSerializer, PostSerializer
+        # from .models import Coffee, Post
 
-# # from django.http import HttpResponse
-# # from django.template import loader
+        # class CoffeeViewSet(viewsets.ModelViewSet):
+        #     queryset = Coffee.objects.all().order_by('name')
+        #     serializer_class = CoffeeSerializer
 
-
-# # def coffee(request):
-# #     template = loader.get_template('sample.html')
-# #     return HttpResponse(template.render())
+        # class PostViewSet(viewsets.ModelViewSet):
+        #     queryset = Post.objects.all().order_by('title')
+        #     serializer_class = PostSerializer
